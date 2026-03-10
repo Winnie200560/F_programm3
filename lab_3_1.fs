e4e6f6524a347041fd12ddcc534eb78bedb0aee2
Winnie <epanovaalina@gmail.com>
2026-03-10 21:02:29 +0500

    Add files via upload

---
+++ lab_3_1.fs
@@ -0,0 +1,52 @@
+﻿// Лабораторная №3
+// Задача 1
+// Получить список из сумм цифр натуральных чисел, содержащихся в исходном списке
+
+open System
+
+// Функция для вычисления суммы цифр числа
+let rec SummDigit (n : int) : int =
+    if n = 0 then
+        0
+    else
+        (n % 10) + SummDigit (n / 10)
+
+// Функция для формирования списка из чисел введных с клавиатуры
+let inputSeq (n : int) : seq<int> = 
+    seq {
+    for i in 1..n do
+        printf "Введите число %d: " i
+        let input = Console.ReadLine()
+
+        match Int32.TryParse(input) with
+        | (true, num) ->
+            if num < 0 then
+                printfn "Ошибка! Число должно быть натуральным."
+                yield 0
+            else
+                yield num
+        | (false, _) ->
+                printfn "Ошибка! Введён символ или буква."
+                yield 0
+    }
+
+[<EntryPoint>]
+let main args = 
+    printf "Введите количество чисел в списке: "
+    let input = Console.ReadLine()
+
+    match Int32.TryParse(input) with
+    | (true, n) -> 
+        if n <= 0 then 
+            printfn "Ошибка! Количество должно быть положительным числом!"
+        else
+            let numbers = inputSeq n
+            let result = Seq.map SummDigit numbers |> Seq.toList
+            printfn "Суммы цифр: %A" result
+    | (false, _) -> 
+        printfn "Ошибка! Введите числовое значение!"
+        
+    0
+
+
+
---
+++ lab_3_2.fs
@@ -0,0 +1,56 @@
+﻿// Задача 2
+// Найти сумму тех элементов списка, которые начинаются на заданную цифру.
+
+open System
+
+// Функция для нахождения первой цифры числа
+let rec firstDigit (n : int) : int =
+    let x = abs n
+    if x < 10 then
+        x
+    else 
+        firstDigit (x / 10)
+
+// Фунция ввода списка
+let inputSeq (n : int) : seq<int> = 
+    seq {
+    for i in 1..n do
+        printf "Введите число: "
+        let input = Console.ReadLine()
+
+        match Int32.TryParse(input) with
+        | (true, num) ->
+                yield num
+        | (false, _) ->
+                printfn "Ошибка! Введён символ или буква."
+                yield 0
+    }
+
+[<EntryPoint>]
+let main args = 
+    printf "Введите количество чисел в списке: "
+    let n = Console.ReadLine()
+
+    match Int32.TryParse(n) with
+    | (true, num) ->
+        if num <= 0 then 
+            printfn "Ошибка! Количество должно быть положительным числом!"
+        else
+            let numbers = inputSeq num
+
+            printfn "Введите цифру (0-9): "
+            let digit = int(Console.ReadLine())
+            let result = 
+                Seq.fold 
+                    (fun sum x -> 
+                        if firstDigit x = digit then 
+                            sum + x 
+                        else 
+                            sum) 
+                    0 numbers
+            printfn "Сумма элементов, начинающихся на %d = %d" digit result
+
+    | (false, _) ->
+                printfn "Ошибка! Введён символ или буква."
+
+    0
