f9c2fb8d62394d937acc1c44fb587f2f6dc4ed0e
Winnie <epanovaalina@gmail.com>
2026-03-11 08:10:58 +0500

    Add files via upload

---
+++ lab_3_3.fs
@@ -0,0 +1,27 @@
+﻿// Задача №3
+// Вывести список файлов, имя которых начинается с заданного символа, в указанном каталоге.
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main args = 
+
+    printf "Введите путь к каталогу: "
+    let way = Console.ReadLine()
+
+    printf "Введите начальный символ файла: "
+    let symbol = Console.ReadLine().[0]
+
+    if Directory.Exists(way) then
+
+        for file in Directory.EnumerateFiles(way) do
+            let name = Path.GetFileName(file) // Извлечение имени файла
+
+            if name.StartsWith(string symbol) then
+                printfn "%s" name
+
+    else
+        printfn "Каталог не найден"
+
+    0
